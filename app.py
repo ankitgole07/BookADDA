4e77eaf95b2a2e339fe3c207a8882fd524924d66
Ankit Gole <ankit.gole@moderncoe.edu.in>
2021-05-11 20:28:17 +0530

    Database comment

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'BookADDA'  #Database name for MYSQL
+app.config['MYSQL_DB'] = 'BookADDA'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # init MySQL
